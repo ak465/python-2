eb792d8afc57519c06f4c640e915457122f9d5d2
ak465 <76836592+ak465@users.noreply.github.com>
2021-11-05 16:03:41 +0530

    Update swappingFile.py

--- swappingFile.py
+++ swappingFile.py
@@ -8,9 +8,9 @@ def swapFileData():
     with open (file2,'r') as b:
         data_b = b.read()
 
-    with open(file1, 'w+')as a:
+    with open(file1, 'w')as a:
         a.write(data_b)
-    with open(file2, 'w+')as b:
+    with open(file2, 'w')as b:
         b.write(data_a)
 
 swapFileData()
